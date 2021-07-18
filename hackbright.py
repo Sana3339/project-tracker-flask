176042147fa11d07d7642b97c3b85f7b555dfd49
Sana <sanaahmad393@gmail.com>
2021-07-18 19:08:00 +0000

    Added functionality to add new project

--- hackbright.py
+++ hackbright.py
@@ -72,6 +72,24 @@ def make_new_student(first_name, last_name, github):
     print(f"Successfully added student: {first_name} {last_name}")
 
 
+def make_new_project(title, description, max_grade):
+    """Add a new project to database.
+
+    Given the project's name and the project's description, add the
+    project to the database.
+    """
+
+    QUERY = """
+        INSERT INTO projects (title, description, max_grade)
+            VALUES(:title, :description, :max_grade)
+        """
+
+    db.session.execute(QUERY, {'title': title,
+                              'description': description,
+                              'max_grade': max_grade})
+
+    db.session.commit()
+
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
 
--- hackbright_web.py
+++ hackbright_web.py
@@ -37,12 +37,12 @@ def search_for_student():
 def show_student_creation_form():
     """Show form for creating a new student."""
 
-    return render_template('create_student.html')
+    return render_template("create_student.html")
 
 
 @app.route("/api/create-student", methods=["POST"])
 def create_student():
-    """Get values from form and save in database."""
+    """Get values from form and save new student in database."""
 
     first_name = request.form.get('first_name')
     last_name = request.form.get('last_name')
@@ -50,16 +50,35 @@ def create_student():
 
     hackbright.make_new_student(first_name, last_name, github)
 
-    return render_template('confirmation_template.html', first=first_name,
+    return render_template("confirmation_template.html", first=first_name,
                                                             last=last_name,
                                                             github=github)
+
+@app.route("/create-project")
+def show_project_creation_form():
+    """Displays form for user to complete to create a new project."""
+
+    return render_template("create_project.html")
+
+
+@app.route("/api/create-project", methods=["POST"])
+def create_project():
+    "Get values from form and save new project in database."
+
+    title = request.form.get('title')
+    description = request.form.get('description')
+    max_grade = request.form.get('max_grade')
+
+    hackbright.make_new_project(title, description, max_grade)
+
+    return render_template("project_confirmation.html", title=title)
 
 @app.route("/project")
 def show_project_details():
     """Show details of a project along with students and their grades
     on the project."""
 
-    title = request.args.get('project_title')
+    title = request.args.get('title')
 
     title, description, max_grade = hackbright.get_project_by_title(title)
 
